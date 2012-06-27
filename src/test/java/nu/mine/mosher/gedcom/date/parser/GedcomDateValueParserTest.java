ae50219b476c7f40b19ee9ba1e4bf706eec42d83
Christopher Mosher <cmosher01@gmail.com>
2012-06-26 23:02:46 -0400

    add some more date tests

--- src/test/java/nu/mine/mosher/gedcom/date/parser/GedcomDateValueParserTest.java
+++ src/test/java/nu/mine/mosher/gedcom/date/parser/GedcomDateValueParserTest.java
@@ -31,27 +31,30 @@ public class GedcomDateValueParserTest
     {
         final DatePeriod period = parse("1 JAN 2001");
         assertThat(period, is(new DatePeriod(new DateRange(new YMD(2001, 1, 1)))));
+        assertFalse(period.getStartDate().getEarliest().isJulian());
+        assertFalse(period.getStartDate().getEarliest().isCirca());
     }
 
     @Test
     public void nominalRange() throws ParseException, DateRange.DatesOutOfOrder
     {
         final DatePeriod period = parse("BET 3 JUL 1966 AND 3 AUG 1966");
-        assertThat(period, is(new DatePeriod(new DateRange(new YMD(1966, 7, 3),new YMD(1966, 8, 3)))));
+        assertThat(period, is(new DatePeriod(new DateRange(new YMD(1966, 7, 3), new YMD(1966, 8, 3)))));
     }
 
     @Test
     public void nominalPeriod() throws ParseException, DateRange.DatesOutOfOrder
     {
         final DatePeriod period = parse("FROM 3 JUL 1966 TO 3 AUG 1966");
-        assertThat(period, is(new DatePeriod(new DateRange(new YMD(1966, 7, 3)),new DateRange(new YMD(1966, 8, 3)))));
+        assertThat(period, is(new DatePeriod(new DateRange(new YMD(1966, 7, 3)), new DateRange(new YMD(1966, 8, 3)))));
     }
 
     @Test
     public void nominalCirca() throws ParseException, DateRange.DatesOutOfOrder
     {
         final DatePeriod period = parse("ABT 1400");
-        assertThat(period, is(new DatePeriod(new DateRange(new YMD(1400,0,0,true)))));
+        assertThat(period, is(new DatePeriod(new DateRange(new YMD(1400, 0, 0)))));
+        assertTrue(period.getStartDate().getEarliest().isCirca());
     }
 
     @Test
@@ -65,14 +68,14 @@ public class GedcomDateValueParserTest
     public void nominalOnlyMonthAndYear() throws ParseException, DateRange.DatesOutOfOrder
     {
         final DatePeriod period = parse("DEC 1941");
-        assertThat(period, is(new DatePeriod(new DateRange(new YMD(1941,12)))));
+        assertThat(period, is(new DatePeriod(new DateRange(new YMD(1941, 12)))));
     }
 
     @Test
     public void nominalInterpreted() throws ParseException, DateRange.DatesOutOfOrder
     {
         final DatePeriod period = parse("INT 31 MAR 1850     (Easter 1850)");
-        assertThat(period, is(new DatePeriod(new DateRange(new YMD(1850,3,31)))));
+        assertThat(period, is(new DatePeriod(new DateRange(new YMD(1850, 3, 31)))));
     }
 
     @Test
@@ -93,23 +96,24 @@ public class GedcomDateValueParserTest
     public void nominalOS() throws ParseException, DateRange.DatesOutOfOrder
     {
         final DatePeriod period = parse("@#DJULIAN@ 11 FEB 1731/2");
-        assertThat(period, is(new DatePeriod(new DateRange(new YMD(1732,2,22,false,true)))));
+        assertThat(period, is(new DatePeriod(new DateRange(new YMD(1732, 2, 22)))));
+        assertTrue(period.getStartDate().getEarliest().isJulian());
     }
 
     @Test
     public void leadingZero() throws ParseException, DateRange.DatesOutOfOrder
     {
         final DatePeriod period = parse("09 DEC 2000");
-        assertThat(period, is(new DatePeriod(new DateRange(new YMD(2000,12,9)))));
+        assertThat(period, is(new DatePeriod(new DateRange(new YMD(2000, 12, 9)))));
     }
 
-    @Test(expected=ParseException.class)
+    @Test(expected = ParseException.class)
     public void zeroYear() throws ParseException, DateRange.DatesOutOfOrder
     {
         parse("1 JAN 0");
     }
 
-    @Test(expected=ParseException.class)
+    @Test(expected = ParseException.class)
     public void onlyDayAndMonth() throws ParseException, DateRange.DatesOutOfOrder
     {
         parse("25 JUL");
@@ -119,10 +123,18 @@ public class GedcomDateValueParserTest
     public void unlabeledDateWithSlashedYearIsDetectedAsJulian() throws ParseException, DateRange.DatesOutOfOrder
     {
         final DatePeriod period = parse("11 FEB 1731/2");
-        assertThat(period, is(new DatePeriod(new DateRange(new YMD(1732,2,22,false,true)))));
+        assertThat(period, is(new DatePeriod(new DateRange(new YMD(1732, 2, 22)))));
+        assertTrue(period.getStartDate().getEarliest().isJulian());
     }
 
-    @Test(expected=ParseException.class)
+    @Test
+    public void twoSlashedYears() throws ParseException, DateRange.DatesOutOfOrder
+    {
+        final DatePeriod period = parse("FROM 11 FEB 1731/2 TO 12 FEB 1731/2");
+        assertThat(period, is(new DatePeriod(new DateRange(new YMD(1732, 2, 22)), new DateRange(new YMD(1732, 2, 23)))));
+    }
+
+    @Test(expected = ParseException.class)
     public void slashedYearCannotBeGregorian() throws ParseException, DateRange.DatesOutOfOrder
     {
         parse("@#DGREGORIAN@ 11 FEB 1731/2");
@@ -132,13 +144,10 @@ public class GedcomDateValueParserTest
     public void omittedSpaceAfterCalendarEscapeIsAllowed() throws ParseException, DateRange.DatesOutOfOrder
     {
         final DatePeriod period = parse("@#DGREGORIAN@2 FEB 2222");
-        assertThat(period, is(new DatePeriod(new DateRange(new YMD(2222,2,2)))));
+        assertThat(period, is(new DatePeriod(new DateRange(new YMD(2222, 2, 2)))));
     }
 
-/*
-*/
-
-private static DatePeriod parse(final String s) throws ParseException, DatesOutOfOrder
+    private static DatePeriod parse(final String s) throws ParseException, DatesOutOfOrder
     {
         final GedcomDateValueParser parser = new GedcomDateValueParser(
             new StringReader(s));
@@ -146,18 +155,19 @@ private static DatePeriod parse(final String s) throws ParseException, DatesOutO
         return parser.parse();
     }
 }
-/*
- * TODO french, hebrew, other
 
-function nominalHebrew(doh) {
-    doh.is(GedcomDateParser.parse(
-            "@#DHEBREW@ 1 TSH 5771"), 
-            {year:5771,month:7,day:1,hebrew:true});
-}
+/*-
+ TODO other calendars
 
-function unknownCalendar(doh) {
-        doh.is(GedcomDateParser.parse(
-                "@#DNEPALI@ 25 FALGUN 2067"),
-                "NEPALI: 25 FALGUN 2067");
-},
-*/
+ function nominalHebrew(doh) {
+ doh.is(GedcomDateParser.parse(
+ "@#DHEBREW@ 1 TSH 5771"), 
+ {year:5771,month:7,day:1,hebrew:true});
+ }
+
+ function unknownCalendar(doh) {
+ doh.is(GedcomDateParser.parse(
+ "@#DNEPALI@ 25 FALGUN 2067"),
+ "NEPALI: 25 FALGUN 2067");
+ },
+ */
