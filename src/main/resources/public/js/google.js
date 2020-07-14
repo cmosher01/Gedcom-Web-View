6e60d89a7a189563fbbc5d8b3731ed30311410bb
Christopher A. Mosher <cmosher01@gmail.com>
2020-07-14 16:25:05 -0400

    refactor Google js and css

---
+++ src/main/resources/public/js/google.js
@@ -0,0 +1,26 @@
+function onSignIn(googleUser) {
+    var c = Cookies.get('idtoken');
+    if (c === undefined) {
+        document.cookie = "idtoken="+googleUser.getAuthResponse().id_token+" ; Path=/ ; Max-Age=900";
+        window.location.reload(true);
+    }
+}
+
+function signOut() {
+    gapi.auth2.getAuthInstance().signOut();
+    document.cookie = "idtoken= ; Path=/ ; Expires = Thu, 01 Jan 1970 00:00:00 GMT";
+    window.location.reload(true);
+}
+
+function renderButton() {
+    gapi.signin2.render(
+        'gedcom-web-view-google-signin',
+        {
+            'scope': 'profile email',
+            'width': 120,
+            'height': 26,
+            'onsuccess': onSignIn,
+            'onfailure': signOut
+        }
+    );
+}
